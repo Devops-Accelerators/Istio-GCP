15bdeaa596ee86f7d13272c14eef7f05b515af36
DevOps-Accelerators <48236651+Devops-Accelerators@users.noreply.github.com>
2019-03-13 11:20:45 +0530

    Update 01-infra-setup.sh

--- bookinfo/01-infra-setup.sh
+++ bookinfo/01-infra-setup.sh
@@ -1,26 +1,15 @@
 #!bin/bash
 
-gcloud components install kubectl
+kubectl apply -f <(istioctl kube-inject -f samples/bookinfo/platform/kube/bookinfo.yaml)
 
-gcloud config set project gcloudstarter-232905
-gcloud config set compute/zone us-central1-b
+kubectl get services
 
-gcloud container clusters create istio-tutorial \
-    --machine-type=n1-standard-2 \
-    --num-nodes=4 \
+kubectl get pods
 
+kubectl apply -f samples/bookinfo/networking/bookinfo-gateway.yaml
 
-kubectl create clusterrolebinding cluster-admin-binding \
-  --clusterrole=cluster-admin \
-  --user="$(gcloud config get-value core/account)"
-  
- 
- wget https://github.com/istio/istio/releases/download/1.1.0-rc.1/istio-1.1.0-rc.1-linux.tar.gz
- tar -xvf istio-1.1.0-rc.1-linux.tar.gz
- mv istio-1.1.0-rc.1 istio
- rm istio-1.1.0-rc.1-linux.tar.gz
- 
- export PATH=$PATH:$(pwd)/istio/bin
- 
- cd istio
- kubectl apply -f install/kubernetes/istio-demo-auth.yaml
+kubectl get svc istio-ingressgateway -n istio-system
+
+#export GATEWAY_URL=35.239.7.64:80
+
+#curl -I http://${GATEWAY_URL}/productpage
