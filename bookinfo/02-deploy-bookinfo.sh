e321e0918dac2862f3eb5400fc0dcfc86188da43
DevOps-Accelerators <48236651+Devops-Accelerators@users.noreply.github.com>
2019-03-13 11:27:18 +0530

    Update 02-deploy-bookinfo.sh

--- bookinfo/02-deploy-bookinfo.sh
+++ bookinfo/02-deploy-bookinfo.sh
@@ -1,15 +1,25 @@
 #!bin/bash
 
+#Downloading Istio release repositiory and configuring istioctl
+wget https://github.com/istio/istio/releases/download/1.1.0-rc.1/istio-1.1.0-rc.1-linux.tar.gz
+tar -xvf istio-1.1.0-rc.1-linux.tar.gz
+mv istio-1.1.0-rc.1 istio
+rm istio-1.1.0-rc.1-linux.tar.gz
+export PATH=$PATH:$(pwd)/istio/bin 
+
+cd istio
+
+#kubectl apply -f install/kubernetes/istio-demo-auth.yam
+
+#Deploying BookInfo Application to GKE Cluster
 kubectl apply -f <(istioctl kube-inject -f samples/bookinfo/platform/kube/bookinfo.yaml)
 
-kubectl get services
 
-kubectl get pods
-
+#Creating ingress gateway
 kubectl apply -f samples/bookinfo/networking/bookinfo-gateway.yaml
 
 kubectl get svc istio-ingressgateway -n istio-system
 
-#export GATEWAY_URL=35.239.7.64:80
 
+#export GATEWAY_URL=35.239.7.64:80
 #curl -I http://${GATEWAY_URL}/productpage
